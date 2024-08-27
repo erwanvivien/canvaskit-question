566d7942f6f150d3b0856a48541281d91ca823cd
Erwan Vivien <erwan.vivien@epita.fr>
2024-08-27 10:05:05 +0200

    add question

---
+++ README.md
@@ -0,0 +1,15 @@
+# CanvasKit question
+
+Install deps with `npm i`
+
+Start with `npm run dev`
+
+Check ./src/main.ts:25
+
+When the loop has a < 1 in it, we have this:
+
+![Only 1 in the loop](./When1Canvas.png)
+
+When the loop has a < 2 in it, we have that
+
+![2 in the loop](./When2Canvas.png)
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import { getCanvasKit } from "./canvaskit";
+import type { Canvas, Surface } from "canvaskit-wasm";
 
 function assertDefined<T>(value: T): asserts value is NonNullable<T> {
   if (value === undefined || value === null) {
@@ -8,42 +9,68 @@ function assertDefined<T>(value: T): asserts value is NonNullable<T> {
   }
 }
 
+const app = document.getElementById("app");
+assertDefined(app);
+
 const main = async () => {
-  const canvas = document.getElementById("canvas");
-  assertDefined(canvas);
-  if (!(canvas instanceof HTMLCanvasElement)) {
-    throw new Error("Canvas is not an instance of HTMLCanvasElement");
-  }
+  const canvases: {
+    domCanvas: HTMLCanvasElement;
+    skiaCanvas: Canvas;
+    surface: Surface;
+  }[] = [];
 
   const ck = await getCanvasKit();
 
-  const image = new Image();
-  image.src = "/download.png";
-  await new Promise((resolve) => {
-    image.onload = resolve;
-  });
+  // Change the number of canvases to 2 to see the issue
+  // 1 works fine and draws the image with blur then a rounded rectangle
+  // 2 only draws the image with blur then clears and draws the rect
+  for (let i = 0; i < 1; i++) {
+    const canvas = document.createElement("canvas");
+    canvas.id = String(i);
+    assertDefined(canvas);
+    app.appendChild(canvas);
 
-  const surface = ck.MakeWebGLCanvasSurface(canvas);
-  assertDefined(surface);
+    const image = new Image();
+    image.src = "/download.png";
+    await new Promise((resolve) => {
+      image.onload = resolve;
+    });
 
-  const skiaCanvas = surface.getCanvas();
+    const surface = ck.MakeWebGLCanvasSurface(canvas);
+    assertDefined(surface);
+
+    const skiaCanvas = surface.getCanvas();
+
+    canvases.push({
+      domCanvas: canvas,
+      skiaCanvas,
+      surface,
+    });
+
+    const paint = new ck.Paint();
+    const skiaImage = ck.MakeImageFromCanvasImageSource(image);
+
+    skiaCanvas.clear(ck.WHITE);
+
+    // Draw with blur
+    paint.setMaskFilter(ck.MaskFilter.MakeBlur(ck.BlurStyle.Normal, 2, false));
+    skiaCanvas.drawImage(skiaImage, 10, 10, paint);
+    paint.setMaskFilter(null);
+
+    paint.delete();
+    surface.flush();
+  }
 
   const paint = new ck.Paint();
-  // paint.setColor(ck.Color4f(0.9, 0, 0, 1.0));
-  // paint.setStyle(ck.PaintStyle.Stroke);
+  paint.setColor(ck.Color4f(0.9, 0, 0, 1));
+  paint.setStyle(ck.PaintStyle.Stroke);
+  paint.setStrokeWidth(5);
   // paint.setAntiAlias(true);
-  // const rr = ck.RRectXY(ck.LTRBRect(10, 60, 210, 260), 25, 15);
-  const skiaImage = ck.MakeImageFromCanvasImageSource(image);
+  const rr = ck.RRectXY(ck.LTRBRect(10, 60, 210, 260), 25, 15);
 
-  skiaCanvas.clear(ck.WHITE);
-
-  // Draw with blur
-  paint.setMaskFilter(ck.MaskFilter.MakeBlur(ck.BlurStyle.Normal, 2, false));
-  skiaCanvas.drawImage(skiaImage, 10, 10, paint);
-  paint.setMaskFilter(null);
-
+  canvases[0].skiaCanvas.drawRRect(rr, paint);
+  canvases[0].surface.flush();
   paint.delete();
-  surface.flush();
 };
 
 main();
